c2c568309b32866b5fd90663397bfdb8338051bd
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-11-21 10:37:29 +0530

    updated

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
+class choice{
+
+
+    public choice(int n1,int n2, int c) {
+
+        switch (c){
+
+
+        }
+    }
+}
+
 public class Calculator {
+
+
 
     public static void main(String[] args) {
 
+        System.out.println("Enter the first number");
+        Scanner num1 = new Scanner(System.in);
+        int n1 = num1.nextInt();
+        System.out.println("Enter the second number");
+        Scanner num2 = new Scanner(System.in);
+        int n2 = num2.nextInt();
 
+        System.out.println("Enter 1 for Add, 2 for Subtraction, 3 for Multiplication or 4 for division");
 
+        Scanner ch = new Scanner(System.in);
+        int c = ch.nextInt();
+
+        choice cho = new choice(n1,n2,c);
     }
 }
